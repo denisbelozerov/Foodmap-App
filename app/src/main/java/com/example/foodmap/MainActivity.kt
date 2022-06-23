5b64353fd3476970d7bbb4778a57b70a5301f2c0
Denis Belozerov <malooo@yandex.ru>
2022-06-24 00:44:22 +0500

    1) new layout custom alert dialog 2) add weight of products

--- app/src/main/java/com/example/foodmap/MainActivity.kt
+++ app/src/main/java/com/example/foodmap/MainActivity.kt
@@ -4,11 +4,8 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.widget.SimpleCursorAdapter
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodmap.databinding.ActivityMainBinding
@@ -36,7 +33,7 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
             productCursor.moveToPosition(i)
             val nameProduct: String = productCursor.getString(productCursor.getColumnIndexOrThrow(DatabaseHelper.columnProductsName))
             val categoryProduct: String = productCursor.getString(productCursor.getColumnIndexOrThrow(DatabaseHelper.columnCategoryName))
-            productList.add(ProductModel(nameProduct, categoryProduct)) // сюда надо ссылку на название продукта и категорию по нужной строке
+            productList.add(ProductModel(nameProduct, categoryProduct))
             i++
         }
         return productList
@@ -50,8 +47,8 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         val itemFructose = fodmapCursor.getString(fodmapCursor.getColumnIndexOrThrow(DatabaseHelper.columnFodmapFructose))
         val itemPolyols = fodmapCursor.getString(fodmapCursor.getColumnIndexOrThrow(DatabaseHelper.columnFodmapPolyols))
         val itemLactose = fodmapCursor.getString(fodmapCursor.getColumnIndexOrThrow(DatabaseHelper.columnFodmapLactose))
-        val itemGluten = fodmapCursor.getString(fodmapCursor.getColumnIndexOrThrow(DatabaseHelper.columnProductsGluten))
-        createAlertDialog("${product.product}", itemOlygos, itemFructose, itemPolyols, itemLactose, itemGluten)
+        val itemWeight = fodmapCursor.getString(fodmapCursor.getColumnIndexOrThrow(DatabaseHelper.columnFodmapUnit))
+        createAlertDialog("${product.product}", itemOlygos, itemFructose, itemPolyols, itemLactose, itemWeight)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,10 +65,10 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         db = databaseHelper.open()
 
         val queryProducts = "SELECT * FROM " + DatabaseHelper.tableProducts + " INNER JOIN " + DatabaseHelper.tableCategory + " ON " + "Продукты.[Категория продукта] = Категория.[Код категории]"
-        productCursor = db.rawQuery(queryProducts, null)
-
         val queryFodmap = "SELECT Продукты._id, Продукты.[Название продукта], Продукты.[Наличие глютена], FODMAP.[Единицы измерения гр], FODMAP.[Наличие Olygos (олигосахариды)], FODMAP.[Наличие Fructose (фруктоза)], FODMAP.[Наличие Polyols (полиолы)], FODMAP.[Наличие Lactose (лактоза)]\n" +
                 "FROM Продукты INNER JOIN FODMAP ON Продукты.[Название продукта] = FODMAP.[Название продукта]" // запрос к БД на получение информации о продуктах
+
+        productCursor = db.rawQuery(queryProducts, null)
         fodmapCursor = db.rawQuery(queryFodmap,  null)
 
         initial()
@@ -83,26 +80,22 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         productCursor.close()
     }
 
-
     // Создание Alert Dialog
-    private fun createAlertDialog(title: String, olygos: String, fructose: String, polyols: String, lactose: String, gluten: String) {
-        val productDialogView = LayoutInflater.from(this).inflate(R.layout.custom_alert_dialog_info_products, null)
-        val builder = AlertDialog.Builder(this).setView(productDialogView).setTitle(title) // AlertDialogBuilder
+    private fun createAlertDialog(title: String, olygos: String, fructose: String, polyols: String, lactose: String, weight: String) {
+        val productDialogView = LayoutInflater.from(this).inflate(R.layout.custom_alert_dialog_info_products_1, null)
+        val builder = AlertDialog.Builder(this).setView(productDialogView).setTitle(title)
 
-        val textViewOlygos: TextView = productDialogView.findViewById(R.id.Olygos)
-        textViewOlygos.setText("Olygos: " + olygos)
+        val textViewWeight: TextView = productDialogView.findViewById(R.id.Product_weight_1_1_1)
+        val textViewOlygos: TextView = productDialogView.findViewById(R.id.Olygos_1_1_2)
+        val textViewFructose: TextView = productDialogView.findViewById(R.id.Fructose_1_1_3)
+        val textViewPolyols: TextView = productDialogView.findViewById(R.id.Polyols_1_1_4)
+        val textViewLactose: TextView = productDialogView.findViewById(R.id.Lactose_1_1_5)
 
-        val textViewFructose: TextView = productDialogView.findViewById(R.id.Fructose)
-        textViewFructose.setText("Fructose: " + fructose)
-
-        val textViewPolyols: TextView = productDialogView.findViewById(R.id.Polyols)
-        textViewPolyols.setText("Polyols: " + polyols)
-
-        val textViewLactose: TextView = productDialogView.findViewById(R.id.Lactose)
-        textViewLactose.setText("Lactose: " + lactose)
-
-        val textViewGluten: TextView = productDialogView.findViewById(R.id.Gluten)
-        textViewGluten.setText("Наличие глютена: " + gluten)
+        textViewWeight.setText(weight + " гр")
+        textViewOlygos.setText(olygos)
+        textViewFructose.setText(fructose)
+        textViewPolyols.setText(polyols)
+        textViewLactose.setText(lactose)
 
         builder.setPositiveButton("OK") { dialog, which ->
         }
